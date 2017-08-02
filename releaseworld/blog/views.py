b68b41010fa61b0e54dfb28ddc7685a56adb244d
JimJeon <jimjeon.me@gmail.com>
2017-08-02 23:16:43 +0900

    Temporary commit : 나 지금 포맷한다. 고로 잠시 업로드를 한다.

--- releaseworld/blog/views.py
+++ releaseworld/blog/views.py
@@ -1,2 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import login
 
+
+def BlogHome(request, username):
+    user = request.user
+
+    return render('home.html', { 'user':user })
+
+def LoginCheck(request):
+    if request.user.is_authenticated():
+        return redirect('BlogHome', username=request.user.username)
+    return redirect('../accounts/login/')
