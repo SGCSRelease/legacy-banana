69869c3151839d6d8f3d4622cd20ce859fc66994
JimJeon <jimjeon.me@gmail.com>
2017-08-05 03:56:02 +0900

    url '/blog/<username>/' created for url, view blog_home added. And view login_check is for redirection from /blog/ to /accounts/login/ only for unauthorized users.

--- releaseworld/blog/urls.py
+++ releaseworld/blog/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from blog.views import LoginCheck, BlogHome
+from blog.views import login_check, blog_home
 
 
 urlpatterns = [
-    url(r'^$', LoginCheck, name='login_check'),
-    url(r'^<username>/$', BlogHome, name='blog_home'), 
+    url(r'^$', login_check, name='login_check'),
+    url(r'^(?P<username>\w+)/$', blog_home, name='blog_home'),
 ]
--- releaseworld/blog/views.py
+++ releaseworld/blog/views.py
@@ -1,13 +1,16 @@
+from django.urls.base import reverse
 from django.shortcuts import render, redirect
-from django.contrib.auth.views import login
+from django.contrib.auth.models import User
 
 
-def BlogHome(request, username):
-    user = request.user
+def blog_home(request, username):
+    user = User.objects.get(username=request.user)
 
-    return render('home.html', { 'user':user })
+    return render(request, 'blog/home.html', {'user':user})
 
-def LoginCheck(request):
+def login_check(request):
     if request.user.is_authenticated():
-        return redirect('BlogHome', username=request.user.username)
+        username = str(request.user)
+        return redirect('./blog_home', username=username)
     return redirect('../accounts/login/')
+
